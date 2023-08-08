e6b9398391adc9721c51cb863ce01bea7f39110c
Hal@0401 <110326199+Hal-0401@users.noreply.github.com>
2023-08-08 10:09:37 +0900

    Add files via upload

--- cording1/js/js.js
+++ cording1/js/js.js
@@ -2,7 +2,7 @@
 $(function() {
     $('.header-btn').on('click', function(){// .btnをクリックすると〜
         $('body').toggleClass('open');// .btnにcloseというクラスが追加される
-        
+
       });
     });
 
@@ -35,41 +35,47 @@ $(function() {
         $('.header-list li').removeClass('active');
       }
     });
+  
 
-    $(function() {
-      var count =$("#slider-item li").length;
+    $('.slider').slick({
+      fade:true,//切り替えをフェードで行う。初期値はfalse。
+      autoplay: true,//自動的に動き出すか。初期値はfalse。
+      autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+      speed:1000,//スライドの動きのスピード。初期値は300。
+      infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+      slidesToShow: 1,//スライドを画面に3枚見せる
+      slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+      arrows: true,//左右の矢印あり
+      prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+      nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+      dots: true,//下部ドットナビゲーションの表示
+          pauseOnFocus: false,//フォーカスで一時停止を無効
+          pauseOnHover: false,//マウスホバーで一時停止を無効
+          pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+  });
 
-      var current = 1;
+  //スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+  $('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+      $('.slider').slick('slickPlay');
+  });
 
-      var next = 2;
+  $(function() {
 
-      var interval = 3000;
-
-      var duration = 500;
-
-      var timer;
-
-      $("#slider-item li:not(:first-child)").hide();
-
-      timer = setInterval(slideTimer , interval);
-      function slideTimer(){
-        $("#slider-item li:nth-child(" + current + ")").fadeOut(duration);
-        $("#slider-item li:nth-child(" + next + ")").fadeIn(duration);
-
-        current = next;
-        next = ++next;
-        if(next > count) {
-          next = 1;
+    $(window).on("scroll", function() {
+        if ($(this).scrollTop() > 100) {
+            $(".top-btn").fadeIn(300);
+            } else {
+            $(".top-btn").fadeOut(300);
         }
-        $("#slider-btn li a").removeClass("target");
-        $("#slider-btn li:nth-child("+ current +") a")
-        .addClass("target");
-      }
-      $("#slider-btn li a").click(function(){
-        next =$(this).html();
-        clearInterval(timer);
-        timer = setInterval(slideTimer, interval);
-        slideTimer();
-        return false;
-      });
     });
+
+    $(window).on("scroll", function() {
+      if ($(this).scrollTop() > 100) {
+          $(".header-nonidex").fadeIn(300);
+          } else {
+          $(".header-nonidex").fadeOut(300);
+      }
+  });
+
+});
+
